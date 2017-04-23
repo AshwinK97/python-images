85c0965b3061b5a0d27ebe689c32c9ba1f78538c
AshwinK97 <ashwin.kamalakannan@uoit.net>
2017-04-23 06:42:26 -0400

    pixel modifying uses key value

--- image.py
+++ image.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import numpy as np
 
-key = 123;
+key = 5;
 img = Image.open("face.jpg")
 pixels = img.load()
 
@@ -10,9 +10,9 @@ for i in range(img.size[0]):
     for j in range(img.size[1]):
         r, g, b = pixels[i, j]
         if i % 2 == 0:
-            pixels[i, j] = (r*5, g*5, b*5)
+            pixels[i, j] = (r*key, g*key, b*key)
         else:
-            pixels[i, j] = (r/5, g/5, b/5)
+            pixels[i, j] = (r/key, g/key, b/key)
         # pixels[i, j] = (i, j, 100)
 
 img.save("image_out.jpg")
